91c6ae497235f855e676e90aa358cf51b481615a
Keinlinks <kevin.clift.b@hotmail.com>
2023-08-26 16:19:32 -0400

    frontend listo

--- src/app/Pages/chat-page/chat-page/chat-page.component.ts
+++ src/app/Pages/chat-page/chat-page/chat-page.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MessageServiceService } from './message-service.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-chat-page',
   templateUrl: './chat-page.component.html',
   styleUrls: ['./chat-page.component.css'],
 })
-export class ChatPageComponent {
-  constructor(private service: MessageServiceService) {
+export class ChatPageComponent implements OnInit, OnDestroy {
+  popup_show: boolean = false;
+
+  constructor(
+    private service: MessageServiceService,
+    private authService: AuthService,
+    private router: Router,
+    private socket: Socket
+  ) {
     service.getMessage().subscribe((data: string) => {
-      this.messages.push(data);
-      this.reverseArray();
+      this.messagesCopy.push(data);
+      this.messages = this.messagesCopy;
+      this.messages = this.messages.reverse();
     });
   }
+
   messages: string[] = [];
-  reverseArray() {
-    this.messages.reverse();
+  messagesCopy: string[] = [];
+
+  logout() {
+    this.authService.logoutUser();
+    this.router.navigateByUrl('/auth/login');
+  }
+  ngOnInit(): void {
+    this.socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+  }
+  ngOnDestroy(): void {
+    this.socket.disconnect();
   }
 }
